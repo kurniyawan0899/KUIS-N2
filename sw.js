26b76d1a4887518798bdcd1af79efa442ad76ed0
kurniyawan0899 <kurniyawan0899@gmail.com>
2026-09-24 02:21:55 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kanji-n2-v3';
+const CACHE='kanji-n2-v4';
 const AS=['./','./index.html','./data.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(AS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
